1a91d6d2d4c7eed532236460779d8bcbf8b48150
Jose Ramon Vieites Romay <jose.vieites@altia.es>
2022-05-12 01:34:36 +0200

    Set leaflet map as background

---
+++ index.js
@@ -0,0 +1,18 @@
+const getRandomNumber = (maxNumber) => {
+  return Math.random() * maxNumber;
+};
+
+const getRandomCoord = () => {
+  const firstCoord = getRandomNumber(70);
+  return firstCoord - getRandomNumber(70);
+};
+
+var map = L.map('map').setView([getRandomCoord(), getRandomCoord()], 6);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'your.mapbox.access.token',
+}).addTo(map);
