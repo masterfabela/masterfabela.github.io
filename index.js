a40e5ef2d2a69511bd318cfe217a2f2cfaa0c1f1
Jose Ramon Vieites Romay <jose.vieites@altia.es>
2022-05-13 02:17:52 +0200

    Try to solve tile border line on render

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+window.L_DISABLE_3D = true;
 const getRandomNumber = (maxNumber) => {
   return Math.random() * maxNumber;
 };
@@ -21,22 +22,18 @@ const getRandomCoords = () => {
   return [getRandomArbitrary(-100, 100), getRandomArbitrary(-100, 100)];
 };
 
-var map = L.map('map').setView(getRandomCoords(), 14);
+var map = L.map('map').setView(getRandomCoords(), 13);
 const satellite = L.tileLayer(
-  'https://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png',
-  { subdomains: ['otile1', 'otile2', 'otile3', 'otile4'] }
+  'https://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png'
 );
 
 L.tileLayer(
   'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
   {
-    attribution: '',
-    maxZoom: 14,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
+    detectRetina: true,
   }
 ).addTo(map);
+
 /*
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '',
@@ -51,3 +48,33 @@ moveMapToRandomWithAnimation(map, duration);
 setInterval(() => {
   moveMapToRandomWithAnimation(map, duration);
 }, duration * 1000);
+
+const blurLayer = document.querySelector('#map .blur');
+const generalContainer = document.querySelector('#map .general-container');
+
+generalContainer.addEventListener('mouseenter', () => {
+  blurLayer.classList.add('active');
+});
+
+generalContainer.addEventListener('mouseleave', () => {
+  blurLayer.classList.remove('active');
+});
+
+/*
+ * Workaround for 1px lines appearing in some browsers due to fractional transforms
+ * and resulting anti-aliasing.
+ * https://github.com/Leaflet/Leaflet/issues/3575
+ */
+(function () {
+  var originalInitTile = L.GridLayer.prototype._initTile;
+  L.GridLayer.include({
+    _initTile: function (tile) {
+      originalInitTile.call(this, tile);
+
+      var tileSize = this.getTileSize();
+
+      tile.style.width = tileSize.x + 1 + 'px';
+      tile.style.height = tileSize.y + 1 + 'px';
+    },
+  });
+})();
